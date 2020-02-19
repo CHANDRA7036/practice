17a6edd17010e904c52263509d044eb810803ce7
CHANDRA7036 <chandra.iedamakanti@nexgdata.com>
2020-02-19 17:43:32 +0530

    Creation of Todo Application and adding css

--- project01/todo01.js
+++ project01/todo01.js
@@ -1,1 +1,39 @@
-alert('connected');
+// var h = document.createElement(h1);
+// var myValue = document.createTextNode("Hello World!");
+// h.appendChild(myValue);
+// document.querySelector('h1').appendChild(h);
+
+
+function addItem() {
+    var input = document.getElementById('input');
+    var item = input.value;
+    ul = document.getElementById('list');
+    var testnode = document.createTextNode(item)
+
+    if (item === '') {
+        return false;
+
+    } else {
+        //create li 
+
+        li = document.createElement('li')
+
+        //create checkbox
+
+        var checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.setAttribute('id', 'check');
+
+        //create label
+
+        var label = document.createElement('label');
+        label.setAttribute('for', 'item');
+
+        ul.appendChild(label);
+        li.appendChild(checkbox);
+        label.appendChild(testnode);
+        li.appendChild(label);
+        ul.insertBefore(li, ul.childNode[0]);
+        li.className = 'visual';
+    }
+}
