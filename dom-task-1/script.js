0cc1773b19c2fcb752b1c32314ba1fa7deccf21d
CHANDRA7036 <chandra.iedamakanti@nexgdata.com>
2020-02-25 17:58:54 +0530

    display numbers(odd and even) dinamically

--- dom-task-1/script.js
+++ dom-task-1/script.js
@@ -1,34 +1,24 @@
-let numbers = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten'];
-var first = numbers[2];
-for (let i = 0; i < numbers.length; i++) {
-    document.write(numbers[i] + "<br/>");
-    document.getElementById("demo").innerHTML = numbers[i];
-}
+function main(e) {
+    console.log(e);
 
-function odd() {
-    let numbers = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten'];
-    for (let i = 0; i < numbers.length; i++) {
-        if (i % 2 == 0) {
-            document.write(numbers[i] + "<br/>");
-        }
+    // var x = e.odd;
+    var cname = e.getAttribute('class')
+    document.querySelector(cname);
+    console.log();
+
+
+    for (i = 0; i < e.length; i++) {
+        e[i].style.backgroundColor = "green";
     }
 }
 
-function even() {
-    let numbers = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten'];
-    for (let i = 0; i < numbers.length; i++) {
-        if (i % 2 != 0) {
-            document.write(numbers[i] + "<br/>");
-        }
-    }
-}
 
-function reset() {
-    let numbers = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten'];
-    for (let i = 0; i < numbers.length; i++) {
-        if (this.id == "checkTree") {
-            document.write('');
 
-        }
-    }
-}
+// function main(e) {
+//       var x = document.getElementsByClassName("odd");
+
+//       var i;
+//       for (i = 0; i < x.length; i++) {
+//         y[i].style.backgroundColor = "green";
+//       }
+//     }
